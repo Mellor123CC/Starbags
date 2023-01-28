1f91326d3305fe141e4deebf5fa407b4819a624b
Mellor <116329769+Mellor123CC@users.noreply.github.com>
2023-01-28 22:23:03 +0500

    Update

--- StarBeta.lua
+++ StarBeta.lua
@@ -1,7 +1,7 @@
-local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/Robojini/White-List/main/DataBase", true))()
+local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/Starbags/main/DataBase.lua", true))()
 
 if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
-  print("yellow blue vase") -- Ваш скрипт
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/Starbags/main/starbag.lua"))()
 else
   game:service('Players').LocalPlayer:Kick('Bruh')
 end
