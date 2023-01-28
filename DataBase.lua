0366b8f85f7046fb0daa439402eb94e33834d457
Mellor <116329769+Mellor123CC@users.noreply.github.com>
2023-01-28 22:21:31 +0500

    Protect

---
+++ DataBase.lua
@@ -0,0 +1,9 @@
+userids = {
+-- [айди игрока в роблоксе] = true,
+[321321321] = true,
+[123231321] = true
+}
+return userids
+
+-- айди игрока это цифры после /users/
+-- https://www.roblox.com/users/123456789/profile
