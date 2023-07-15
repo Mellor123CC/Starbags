35aec5cfca09f0d3bc3853668c544de2322841b0
Mellor <116329769+Mellor123CC@users.noreply.github.com>
2023-07-15 15:42:26 +0500

    Delete StarBeta.lua

--- StarBeta.lua
+++
@@ -1,7 +0,0 @@
-local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/Starbags/main/DataBase.lua", true))()
-
-if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/Mellor123CC/Starbags/main/starbag.lua"))()
-else
-  game:service('Players').LocalPlayer:Kick('Bruh')
-end
